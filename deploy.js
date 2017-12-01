17d9f8c9e5ce60490bda5886d03db7afb208d253
Unknown <artem.nikolaiev@gmail.com>
2017-12-01 15:15:42 +0200

    r

--- deploy.js
+++ deploy.js
@@ -1,4 +1,7 @@
 const Deployer = require('ssh-deploy-release');
+const decompress = require('decompress');
+const decompressTargz = require('decompress-targz');
+const path = require('path');
  
 const options = {
     localPath: './',
@@ -12,6 +15,16 @@ const options = {
 };
  
 const deployer = new Deployer(options);
-deployer.deployRelease(() => {
+deployer.deployRelease((context) => {
+	const archivePath = context.release.path;
+
+	decompress(path.join(archivePath, 'release.tar.gz'), 'dist', {
+	    plugins: [
+	        decompressTargz()
+	    ]
+	}).then(() => {
+	    console.log('Files decompressed');
+	});
+
     console.log('Deployed !')
 });
