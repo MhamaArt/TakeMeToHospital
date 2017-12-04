fd2e0d3276a27c7e4b6614e2f8a29d0fdbed8a2c
Unknown <artem.nikolaiev@gmail.com>
2017-12-04 05:20:00 +0200

    zvSF

--- deploy.js
+++ deploy.js
@@ -12,11 +12,11 @@ new Deployer({
     // privateKeyFile: './key.rsa',
     deployPath: '/home/mhama/project',
     onAfterDeploy(context, done) {
-    	context.logger.log('Move dir to project/build/');
+    	context.logger.log('Move dir to project/project/');
 
-		context.remote.exec('rm -rf /home/mhama/project/build', () => {
+		context.remote.exec('rm -rf /home/mhama/project/project', () => {
 		    context.logger.log('Removed build/');
-	    	context.remote.exec(`cp -R /home/mhama/project/releases/${context.release.tag} /home/mhama/project/build/`, () => {
+	    	context.remote.exec(`cp -R /home/mhama/project/releases/${context.release.tag} /home/mhama/project/project/`, () => {
 	    	    context.logger.log('Copied');
 	    		context.remote.exec('rm -rf /home/mhama/project/releases/', done);
 	    	});
